c7a9169d60ef59c648efc48d385bf57ed34a666f
Neider Gainza Llacer <neidergainza1@gmail.com>
2026-01-01 11:02:16 -0500

    Fix: Readme , Feat: example

---
+++ example/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:general_list/general_list.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const Scaffold(body: MyList(),),
+    );
+  }
+}
+
+
+class MyList extends StatelessWidget{
+  const MyList({
+    super.key
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return   GeneralList<int>(
+      itemBuilder: (context, item){
+        return Card(
+          child: Padding(
+            padding: EdgeInsetsGeometry.all(8),
+            child: Text(item.toString()),
+          ),
+        );
+      }, 
+      getItems: () async* {
+        yield [for(int i = 0; i < 10; i++) i];
+      }
+    );
+  }
+}
+
